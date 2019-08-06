bd6e05f59d80ca677890a1f17ac2319fa974871c
Stanley Wang <s.wang4331@gmail.com>
2019-08-05 22:23:47 -0400

    backtrader

--- testing/Backtrader-MainBacktester.py
+++ testing/Backtrader-MainBacktester.py
@@ -11,6 +11,7 @@ def install(package):
 install("backtrader")
 from datetime import datetime
 import backtrader as bt
+import backtrader.analyzers as btanalyzers
 
 # Create a subclass of Strategy to define the indicators and logic
 class SmaCross(bt.Strategy):
@@ -33,40 +34,6 @@ class SmaCross(bt.Strategy):
         elif self.crossover < 0:  # in the market & cross to the downside
             self.close()  # close long position
 
-def printTradeAnalysis(analyzer):
-    '''
-    Function to print the Technical Analysis results in a nice format.
-    '''
-    #Get the results we are interested in
-    total_open = analyzer.total.open
-    total_closed = analyzer.total.closed
-    total_won = analyzer.won.total
-    total_lost = analyzer.lost.total
-    win_streak = analyzer.streak.won.longest
-    lose_streak = analyzer.streak.lost.longest
-    pnl_net = round(analyzer.pnl.net.total,2)
-    strike_rate = (total_won / total_closed) * 100
-    #Designate the rows
-    h1 = ['Total Open', 'Total Closed', 'Total Won', 'Total Lost']
-    h2 = ['Strike Rate','Win Streak', 'Losing Streak', 'PnL Net']
-    r1 = [total_open, total_closed,total_won,total_lost]
-    r2 = [strike_rate, win_streak, lose_streak, pnl_net]
-    #Check which set of headers is the longest.
-    if len(h1) > len(h2):
-        header_length = len(h1)
-    else:
-        header_length = len(h2)
-    #Print the rows
-    print_list = [h1,r1,h2,r2]
-    row_format ="{:<15}" * (header_length + 1)
-    print("Trade Analysis Results:")
-    for row in print_list:
-        print(row_format.format('',*row))
-
-def printSQN(analyzer):
-    sqn = round(analyzer.sqn,2)
-    print('SQN: {}'.format(sqn))
-
 
 cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
 
@@ -76,12 +43,17 @@ data = bt.feeds.YahooFinanceData(dataname='MSFT',
                                  todate=datetime(2012, 12, 31))
 
 cerebro.adddata(data)  # Add the data feed
-
 cerebro.addstrategy(SmaCross)  # Add the trading strategy
+# Analyzer
+cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='mysharpe')
+
+thestrats = cerebro.run()
+thestrat = thestrats[0]
+
+print('Sharpe Ratio:', thestrat.analyzers.mysharpe.get_analysis())
+
 cerebro.run()  # run it all
 cerebro.plot()  # and plot it with a single command
 
-# print the analyzers
-printTradeAnalysis(SmaCross.analyzers.ta.get_analysis())
-printSQN(SmaCross.analyzers.sqn.get_analysis())
+
 
