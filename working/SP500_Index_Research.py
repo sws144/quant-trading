5f0acf983acf5e238354e636e8a98998314ff8e3
Stanley Wang <s.wang4331@gmail.com>
2019-08-29 21:38:36 -0400

    finished plotting

--- working/SP500_Index_Research.py
+++ working/SP500_Index_Research.py
@@ -16,6 +16,9 @@ register_matplotlib_converters()
 # more plotting
 import seaborn as sns
 
+# file export
+import openpyxl
+
 # %% 2 import & explore data
 df = pd.read_excel('SP500_Index_Research_FinancialMarketData.xlsx', 
                    sheet_name = "YearlyMacro")
@@ -34,35 +37,50 @@ ax.plot(df['DateFraction'],df['Price'])
 # %% 3 create columns to complete analysis
 
 # future returns (dependent variable)
-df['SP500FwdYr01'] = df['Price'].shift(-12)
+df['SP500FwdYr01'] = df['Price'].shift(-12) #next year
 df['SP500FwdYr01Returns']  = df['SP500FwdYr01'] /df['Price'] - 1
 
-df['SP500FwdYr10'] = df['Price'].shift(-120)
+df['SP500FwdYr10'] = df['Price'].shift(-120) #next 10 yrs
 df['SP500FwdYr10Returns']  = (df['SP500FwdYr10'] /df['Price'])**(.1) - 1
 
 # pick independent variables
+df['InflationTrailing5yrFactor'] = df['CPI'] / df['CPI'].shift(60) #check previous 
+
 df['PERatio'] = df['Price'] / df['Earnings']
-df['Earnings10yr'] = df['Earnings'].rolling(window = 120).mean() 
-df['Earnings10yrGrowthRate'] = .03 #(df['Earnings']/df['Earnings'].shift(-120))**(.1)-1
-df['Earnings10yrAdj'] = df['Earnings10yr'] * (1+df['Earnings10yrGrowthRate'])**5
-## TODO DO INFLATION ADJUSTED
+df['Earnings10yr'] = df['Earnings'].rolling(window = 120).mean()  # last 10 yrs
+df['Earnings10yrGrowthRate'] = (df['Earnings']/df['Earnings'].shift(120))**(.1)-1 # last 10 yrs
+df['Earnings10yrAdj'] = df['Earnings10yr'] * (1+df['Earnings10yrGrowthRate'])**5 \
+    * df['InflationTrailing5yrFactor']
+
 
 # such as target price 
 df['Targetprice1yr']  = df['Earnings'] /(df['RateGS10']/100)
 df['Targetprice1yrReturn'] = df['Targetprice1yr'] /df['Price']  -1 
 df['Targetprice10yr']  = df['Earnings10yrAdj'] /(df['RateGS10']/100)
-df['Targetprice10yrReturn'] = df['Targetprice10yr'] /df['Price']  -1 
+df['Targetprice10yrReturn'] = (df['Targetprice10yr'] /df['Price'])**(.1)  -1 
 
 
 #check results
 print(df.head())
 
 # %% 4 plot data
-fig2, ax2  = plt.subplots()
-ax2.plot(df['PERatio'],df['SP500FwdYr01Returns']) #to fix
+#fig2, ax2  = plt.subplots()
+#ax2.plot(df['PERatio'],df['SP500FwdYr01Returns']) #to fix
 sns.pairplot(df[['DateFraction','RateGS10','PERatio']])
-sns.pairplot(df[['DateFraction', 'RateGS10', 'Targetprice10yrReturn','SP500FwdYr01Returns',
-                 'SP500FwdYr10Returns',]])
+sns.pairplot(df[['DateFraction', 'RateGS10',
+                 'SP500FwdYr10Returns','Earnings10yrGrowthRate',
+                 'InflationTrailing5yrFactor']])
+
+fig3, ax3  = plt.subplots()
+sns.lineplot(df['DateFraction'],df['Targetprice10yrReturn'], color = 'b')
+#ax3 = plt.twinx()
+sns.lineplot(df['DateFraction'],df['SP500FwdYr10Returns'], color = 'g')
+sns.lineplot(df['DateFraction'],df['InflationTrailing5yrFactor']**.2-1, color = 'r')
+sns.lineplot(df['DateFraction'],df['RateGS10']/100, color = 'y')
+fig3.legend(labels=['Targetprice10yrReturn','SP500FwdYr10Returns',
+                    'InflationTrailing5yrFactorAsRate', 'RateGS10' ])
+
+df.to_excel('SP500_Index_Research_Results.xlsx', sheet_name = 'sheet1',)
 
 # check relationship with 
 
